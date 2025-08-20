8511ffecf2651ef0a2a00301cfa68cc1286a529a
stlawton <lawtons347@gmail.com>
2025-08-20 14:50:30 -0400

    Update sudoku_block.py
    Started code, needs debugging

--- src/sudoku_block.py
+++ src/sudoku_block.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def block_correct(sudoku: list, row_no: int, column_no: int):
+  checked_numbers = []
+  current_row = row_no
+  while current_row < row_no + 3:
+    number = 1
+    long_row = sudoku[current_row]
+    while number < 10:
+      short_row = long_row[column_no : column_no + 3]
+      if number in short_row:
+        if checked_numbers.count(number) < 2:
+          checked_numbers.append(number)
+        else:
+          return False
+      number += 1
+    current_row += 1
+  return True
